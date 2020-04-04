f85ce920fbca8403b60388951dc1655313a9270c
Adam Miribyan <adam@bysestra.com>
2020-04-04 04:58:18 +0200

    Test against the search and query recording requests.

--- spec/requests/search/articles_request_spec.rb
+++ spec/requests/search/articles_request_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe "Search::Articles", type: :request do
-
-  describe "GET /index" do
-    it "returns http success" do
-      get "/search/articles/index"
+RSpec.describe Search::ArticlesController, type: :controller do
+  describe 'GET /index' do
+    it 'responds successfully' do
+      get :index
       expect(response).to have_http_status(:success)
     end
-  end
 
+    it 'JS request returns the search results' do
+      get :index, params: { query: 'how' }, format: :js, xhr: true
+      expect(response).to have_http_status(:success)
+      expect(assigns(:articles).pluck(:title)).to include('How does Oura measure my heart rate?')
+    end
+
+    it 'JS requests creates a Search::Query instance' do
+      random_query = Time.now.to_i
+      get :index, params: { query: random_query }, format: :js, xhr: true
+      expect(Search::Query.where(body: random_query)).to exist
+    end
+  end
 end
